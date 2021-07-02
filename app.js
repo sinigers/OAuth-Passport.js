7dac67fe461211014b74f9e18b9409c092938e21
sinigers <sinigers@yahoo.com>
2021-07-02 10:03:23 +0300

    create routes

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 
-//set up view engine
+// set view engine
 app.set('view engine', 'ejs');
 
-//create home route
+// set up routes
+app.use('/auth', authRoutes);
+
+// create home route
 app.get('/', (req, res) => {
     res.render('home');
 });
 
 app.listen(3000, () => {
-    console.log('app now listening to requests on port 3000');
+    console.log('app now listening for requests on port 3000');
 });
-
